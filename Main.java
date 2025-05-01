9832710f90608e59ee2a44f722a7083ce76418a1
peppee4 <g.giuliani22@campus.uniurb.it>
2025-05-01 18:01:49 +0200

    Aggiunta la classe main

---
+++ Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Creazione del Frame
+        JFrame window = new JFrame();                               
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Impostiamo il comportamento per la chiusura della finestra
+        window.setResizable(false);                   // Facciamo in mdoo che la finestra non possa essere ridimensionata dall'utente
+        window.setTitle("The Pyros Stone");               // Titolo della finestra
+
+        window.setLocationRelativeTo(null);                   // Impostiamo la posizione iniziale della finestra al centro dello schermo
+        window.setVisible(true);
+
+    }
+}
