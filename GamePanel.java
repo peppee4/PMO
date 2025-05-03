6f53072f14b87e8a2b21138e7f7eae1464c14eda
peppee4 <g.giuliani22@campus.uniurb.it>
2025-05-03 10:18:32 +0200

    Aggiunta la classe per la gestione dell'input dell'utente

--- GamePanel.java
+++ GamePanel.java
@@ -4,30 +4,41 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel implements Runnable{
 
     // Impostazione della finestra
-	final int originalTileSize = 16; // 16x16 tile
+	final int originalTileSize = 16;                        // 16x16 tile
 	final int scale = 3;
-	public final int tileSize = originalTileSize * scale; // 48 * 48
-	
+
+	final int tileSize = originalTileSize * scale;          // 48 * 48
 	final int maxScreenCol = 16 ;
 	final int maxScreenRow = 12 ;
-	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-	final int screenHeight = tileSize * maxScreenRow; // 576 pixels 
+	final int screenWidth = tileSize * maxScreenCol;        // 768 pixels
+	final int screenHeight = tileSize * maxScreenRow;       // 576 pixels 
 
 	// FPS
 	int FPS = 60;
 	
+    // Creazione di un gestore degli eventi della tastiera
+    KeyHandler keyH = new KeyHandler();
+
+    // Creiamo il Thread per il flusso del gioco
 	Thread gameThread;
 	
+    // Impostiamo il player nella posizione iniziale e impostiamo la sua velocità iniziale
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    // Costruttore della classe
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));    // Dimensione predefinita del JPanel
         this.setBackground(Color.black);                                    // Colore di sfondo del JPanel
         this.setDoubleBuffered(true);                                 // Per ridurre il flickering
-
+        this.addKeyListener(keyH);                                          // Aggiungiamo il Listener al pannello
+        this.setFocusable(true);                                  
     }
 
     public void startGameThread() {
-		gameThread = new Thread(this);
-		gameThread.start();
+		gameThread = new Thread(this);  // Inizializzazione del Thread
+		gameThread.start();             // Thread start
     }
     
     // Metodo principale del thread del gioco
@@ -60,11 +71,28 @@ public class GamePanel extends JPanel implements Runnable{
         }
     }
 	
+    // Funzione per la gestione del movimento del player
 	public void update(){
-		
+		if(keyH.upPressed == true){
+            playerY -= playerSpeed;
+        }else if(keyH.downPressed == true){
+            playerY += playerSpeed;
+        }else if(keyH.leftPressed == true){
+            playerX -= playerSpeed;
+        }else if(keyH.rightPressed == true){
+            playerX += playerSpeed;
+        }
 	}
 	
+    // Funzione per ridisegnare il player
 	public void paintComponent(Graphics g){
-			
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.WHITE);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
 	} 
 }
---
+++ KeyHandler.java
@@ -0,0 +1,48 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener{
+    public boolean upPressed,
+                   downPressed,
+                   leftPressed,
+                   rightPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    // Funzione per dire cosa fare quando viene premuto un bottone
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W){
+            upPressed = true;
+        }else if(code == KeyEvent.VK_S){
+            downPressed = true;
+        }else if(code == KeyEvent.VK_A){
+            leftPressed = true;
+        }else if(code == KeyEvent.VK_D){
+            rightPressed = true;
+        }
+    }
+
+    // Funzione per dire cosa fare quando viene rilasciato un bottone 
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W){
+            upPressed = false;
+        }else if(code == KeyEvent.VK_S){
+            downPressed = false;
+        }else if(code == KeyEvent.VK_A){
+            leftPressed = false;
+        }else if(code == KeyEvent.VK_D){
+            rightPressed = false;
+        }
+    }
+    
+}
